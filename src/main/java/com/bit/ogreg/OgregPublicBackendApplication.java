ee0cd514260513134477c5592d2f3f3576b5cfff
Albert Ozimek <albert.ozimek@bit.admin.ch>
2023-12-12 10:00:40 +0100

    first commit

---
+++ src/main/java/com/bit/ogreg/OgregPublicBackendApplication.java
@@ -0,0 +1,23 @@
+package com.bit.ogreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OgregPublicBackendApplication {
+
+
+    @GetMapping("/message")
+    public String message() {
+        return "Congratulations. Your app has been deployed successfully in Azure 222";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgregPublicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bit/ogreg/OgregPublicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.ogreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgregPublicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
